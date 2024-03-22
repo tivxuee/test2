4d44879c868e1bbe60101f93dea1d9799681f499
unknown <tivxuee@yahoo.com>
2024-03-22 14:16:29 +0800

    first commit

---
+++ AGILE_LEC/test.js
@@ -0,0 +1,19 @@
+var numberOfstudnetsEjected=0
+
+function sayHi() {
+    alert("kick out remainder");
+    alert("who?");
+}
+
+function ejectstudent() {
+    numberOfstudnetsEjected +=1;
+    displayconsole();
+    displayejected();
+}
+ function displayejected() {
+    alert('the number of: '+numberOfstudnetsEjected);
+ }
+
+ function displayconsole() {
+    prompt("give me a number", 7);
+ }
