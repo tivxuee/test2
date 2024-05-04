3d45ba2911e1b12e10d0546026aa9e5fd011e425
tivxuee <tivxuee@yahoo.com>
2024-05-04 20:49:20 +0800

    Added the sugn-up

--- AGILE_LEC/app/routes.py
+++ AGILE_LEC/app/routes.py
@@ -1,6 +1,7 @@
-from flask import redirect, render_template, url_for, request
+from flask import flash, redirect, render_template, url_for, request
 from app import flaskapp, db
 from app.model import Group, Student
+from .forms import CreateGroupForm
 
 @flaskapp.route('/')
 @flaskapp.route('/groups')
@@ -11,12 +12,51 @@ def groups():
 
 @flaskapp.route('/create')
 def create():
-    return render_template('createGroup.html')
+    form = CreateGroupForm()
+    return render_template('createGroup.html',form=form)
 
 @flaskapp.route('/submit', methods=['POST'])
 def submit():
-    print(request.method)
-    print(request.form)
-    print(request.form['numberOfstudents'])
-    print('submitted!')
-    return redirect(location=url_for('groups')) 
+    form = CreateGroupForm()
+    if not form.validate_on_submit():
+        return render_template('createGroup.html',form=form)
+
+    groupSize = int(form.groupSize.data)
+    studentIDs = [
+        form.student1.data,
+        form.student2.data,
+        form.student3.data,
+        form.student4.data
+    ][:groupSize]
+
+    students=[find_student_By_id(id) for id in studentIDs]
+    
+    if not all(students):
+        return render_template('createGroup.html',form=form)
+    
+    all_unique = len(set(students))==len(students)
+    if not all_unique:
+        flash('All students must be unique','error')
+        return render_template('createGroup.html',form=form)
+
+    all_free=True
+    for student in students:
+        if student.group:
+            flash(f'Student {student} is already in a group','error')
+            all_free=False
+
+    if not all_free:
+        return render_template('createGroup.html',form=form)
+            
+    group=Group()
+    group.students=students
+
+   
+    db.session.add(group)
+    db.session.commit()
+
+def find_student_By_id(id:str):
+    student = Student.query.get(id)
+    if not student:
+        flash(f'Student 1 not found: {id}', 'error')
+    return student
